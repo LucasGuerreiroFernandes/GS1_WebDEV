1b36fcf00f83c22b6ab046b7a865c1ffa1432581
Lucas <lguerreiroms@gmail.com>
2024-11-14 14:03:57 -0300

    Criando o Header e adicionando o botões de troca de fundo

--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,28 @@
 let count = 1;
+let timeNextSlide = 4000
 document.getElementById("radio1").checked = true;
 
+alert("Seja Bem-Vindo")
 setInterval( function(){
     nextImage();
-},4000)
+},timeNextSlide)
+
+if(document.getElementById("btn1")==true){
+    
+}
 
 function nextImage(){
-    count++;
-    if(count>4){
-        count=1;
+    if (document.getElementById("radio1").checked) {
+        count=2
+    }
+    if (document.getElementById("radio2").checked) {
+        count=3
+    }
+    if (document.getElementById("radio3").checked) {
+        count=4
+    }
+    if (document.getElementById("radio4").checked) {
+        count=1
     }
     document.getElementById("radio"+count).checked = true;
 }
