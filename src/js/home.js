6dae5b03ba0d296f9966aa42f95f862d3766e7d5
Lucas <lguerreiroms@gmail.com>
2024-11-14 15:00:46 -0300

    Criando botoes para mudar a cor do fundo

--- src/js/home.js
+++ src/js/home.js
@@ -7,8 +7,14 @@ setInterval( function(){
     nextImage();
 },timeNextSlide)
 
-if(document.getElementById("btn1")==true){
-    
+function btn1_true(){
+    document.body.style.backgroundColor = "rgba(231, 92, 41, 0.658)"
+}
+function btn2_true(){
+    document.body.style.backgroundColor = "#f7c081"
+}
+function btn3_true(){
+    document.body.style.backgroundColor = "#f7f6e4"
 }
 
 function nextImage(){
