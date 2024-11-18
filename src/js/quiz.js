106e2e12fec0df6100f42b0138cb95f1f2e23d7a
Lucas <lguerreiroms@gmail.com>
2024-11-18 13:50:20 -0300

    Criando o quiz no html e css

---
+++ src/js/quiz.js
@@ -0,0 +1,11 @@
+
+function btn1_true(){
+    document.body.style.backgroundColor = "rgba(231, 92, 41, 0.658)"
+}
+function btn2_true(){
+    document.body.style.backgroundColor = "#f7c081"
+}
+function btn3_true(){
+    document.body.style.backgroundColor = "#f7f6e4"
+}
+
