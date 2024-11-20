e30984fb85d7234bbbef891b19417dcd050cb99e
Lucas <lguerreiroms@gmail.com>
2024-11-20 14:23:59 -0300

    Criando a aba Solução

--- src/js/login.js
+++ src/js/login.js
@@ -6,7 +6,7 @@ function submit(){
     var senha_num = document.getElementById("senha").value;
     var check_user = document.getElementById("checkUser");
     var check_senha = document.getElementById("checkSenha");
-    if (usuario_num.length < 7){
+    if (usuario_num == ("adm")){
         mensagem.style.visibility = "visible";
         check_user.style.visibility = "hidden";
     }
@@ -14,7 +14,7 @@ function submit(){
          mensagem.style.visibility = 'hidden';
          check_user.style.visibility = "visible";
     }
-    if(senha_num.length < 8){
+    if(senha_num == ("adm123")){
         mensagemSenha.style.visibility = "visible";
         check_senha.style.visibility = "hidden";
     }
@@ -23,7 +23,7 @@ function submit(){
         check_senha.style.visibility = "visible";
         
     }
-    if(usuario_num.length >= 8 && senha_num.length >= 9){
+    if(usuario_num == ("adm") && senha_num == ("adm123")){
         window.location.href="quiz.html"
     }
 }
--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,11 +1,57 @@
-
 function btn1_true(){
-    document.body.style.backgroundColor = "rgba(231, 92, 41, 0.658)"
+    document.body.style.backgroundColor = "rgba(231, 92, 41, 0.658)";
 }
 function btn2_true(){
-    document.body.style.backgroundColor = "#f7c081"
+    document.body.style.backgroundColor = "#f7c081";
 }
 function btn3_true(){
-    document.body.style.backgroundColor = "#f7f6e4"
+    document.body.style.backgroundColor = "#f7f6e4";
 }
+function enviar() {
+    var check1 = document.getElementById("r1b");
+    var check2 = document.getElementById("r2b");
+    var check3 = document.getElementById("r3a");
+    var check4 = document.getElementById("r4a");
+    var check5 = document.getElementById("r5b");
+    var check6 = document.getElementById("r6a");
+    var check7 = document.getElementById("r7a");
+    var check8 = document.getElementById("r8a");
+    var check9 = document.getElementById("r9b");
+    var check10 = document.getElementById("r10a");
 
+    var pontos = 0;
+    
+    if(check1 && check1.checked){
+        pontos++;
+    }
+    if(check2 && check2.checked){
+        pontos++;
+    }
+    if(check3 && check3.checked){
+        pontos++;
+    }
+    if(check4 && check4.checked){
+        pontos++;
+    }
+    if(check5 && check5.checked){
+        pontos++;
+    }
+    if(check6 && check6.checked){
+        pontos++;
+    }
+    if(check7 && check7.checked){
+        pontos++;
+    }
+    if(check8 && check8.checked){
+        pontos++;
+    }
+    if(check9 && check9.checked){
+        pontos++;
+    }
+    if(check10 && check10.checked){
+        pontos++;
+    }
+    
+    alert('Pontos: ' + pontos );
+    pontos = 0;
+}
